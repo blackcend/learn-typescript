51af491a1b9e7b960a6142ede2bf16ee9ee48187
Blackcend <mrcoeus@hotmail.com>
2017-07-03 11:44:33 +0700

    array

---
+++ 21-Array-Tulpe.ts
@@ -0,0 +1,9 @@
+let arr1: string[] = ['Hello','Hi','Xin chao'];
+let arr2:number[] = [1,2,3,4,5];
+let arr3:any[] = [1,'H',true];
+let arr4: Array<string> = ["aba","cbac"];
+
+console.log(arr1[0]);
+
+// tuple
+let tuple1: [string, number] = ["La Quang Dat",25];
---
+++ disk/21-Array-Tulpe.js
@@ -0,0 +1,8 @@
+"use strict";
+var arr1 = ['Hello', 'Hi', 'Xin chao'];
+var arr2 = [1, 2, 3, 4, 5];
+var arr3 = [1, 'H', true];
+var arr4 = ["aba", "cbac"];
+console.log(arr1[0]);
+var tuple1 = ["La Quang Dat", 25];
+//# sourceMappingURL=21-Array-Tulpe.js.map
